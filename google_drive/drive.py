ce00a04a41741b79ed8fa7a73a2afda21bf2f2f6
yusufjonc07 <ahmedovyusufjon3@gmail.com>
2023-11-28 15:56:52 +0500

    commenting

--- google_drive/drive.py
+++ google_drive/drive.py
@@ -6,93 +6,55 @@ from fastapi import APIRouter, UploadFile, File
 from fastapi.responses import FileResponse
 from tempfile import NamedTemporaryFile
 
-"""
-Wrapper class for oauth2client library in google-api-python-client
-"""
+# Authenticate with Google
 gauth = GoogleAuth()
 
-"""
-API Router which has to be included app in main.py file
-"""
+# Create an APIRouter for Google Drive operations
 drive_router = APIRouter(tags=["GOOGLE DRIVE EXAMPLES"])
 
-"""
-connect to the google with downloaded credentials client id
-authentification flow will be available after app runs
-"""    
+# Event to connect to Google Drive on application startup
 @drive_router.on_event("startup")
 async def connect_google():
     gauth.LocalWebserverAuth()
 
-"""
-simple example of getting list of files on the google drive
-"""
+# Endpoint to list files in Google Drive
 @drive_router.get("/files")
 async def list_of_files():
     drive = GoogleDrive(gauth)
+    # Retrieve a list of files in the root directory (not trashed)
+    file_list = [{"id": file['id'], "filename": file['title']} for file in drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()]
+    return file_list
 
-    """
-    title and id of the file are loaded and appended to the empty list
-    """
-    filel_ist = [{"id": file['id'], "filename": file['title']} for file in drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()]
-    
-    """return the list of fileNames and fileIds"""
-    return filel_ist
-
-"""
-simple example of uploading file 
-using FastAPI UploadFile to the Google Drive
-"""
+# Endpoint to upload a file to Google Drive
 @drive_router.post('/upload_file')
-async def upload_file_to_drive(
-   file: UploadFile = File(...)
-):
+async def upload_file_to_drive(file: UploadFile = File(...)):
     drive = GoogleDrive(gauth)
-    """Creating new file and uploading it"""
-    file1 = drive.CreateFile({'title': file.filename})  # Create GoogleDriveFile instance with title 'Hello.txt'.
-    
-    """we use the io bytes so that the content of the file will be flexible for google file content"""
+    file1 = drive.CreateFile({'title': file.filename})
     file_content = io.BytesIO(await file.read())
-    file1.content = file_content # Set content of the file
-    
-    """ Main function which uploads the filled file to the Drive """
+    file1.content = file_content
     file1.Upload()
-    
-    """ Make user aware of the success """
     return 'success'
 
-
-"""
-In this route, you can download the file 
-using its ID which was taken in the GET request above
-"""
+# Endpoint to download a file from Google Drive
 @drive_router.get('/download_file')
-async def download_file_from_drive(
-   fileId: str
-):
-
+async def download_file_from_drive(fileId: str):
     drive = GoogleDrive(gauth)
-
     file1 = drive.CreateFile({'id': fileId})
-    file1.FetchMetadata() 
+    file1.FetchMetadata()
+    # Save the file content to a temporary file
     with NamedTemporaryFile(delete=False) as temp_file:
         file1.GetContentFile(temp_file.name)
-        
+    # Return the temporary file as a response
     return FileResponse(temp_file.name, filename=file1['title'])
-        
-    
 
+# Endpoint to delete a file from Google Drive
 @drive_router.delete('/delete_file')
-async def delete_file_in_drive(
-   fileId: str
-):
+async def delete_file_in_drive(fileId: str):
     try:
         drive = GoogleDrive(gauth)
-
-        file1 = drive.CreateFile({'id': fileId})  # 
+        file1 = drive.CreateFile({'id': fileId})
         file1.FetchMetadata()
         file1.Delete()
-        
         return 'success'
     except ApiRequestError as e:
         print(e)
